fe6424b7a53ebe08cc53fc2020b8edfd47707523
untxi <samarburola@gmail.com>
2014-09-19 22:09:06 -0600

    Fin Cuenta Ahorro Fin Partes 1-4

--- src/logicaDeNegocios/CuentaAhorro.java
+++ src/logicaDeNegocios/CuentaAhorro.java
@@ -1,5 +1,7 @@
 package logicaDeNegocios;
 
+import java.util.Calendar;
+
 public class CuentaAhorro extends Cuenta
 {
 	// Atributos
@@ -14,10 +16,35 @@ public class CuentaAhorro extends Cuenta
 	public double getCoutaMantenimiento() {return coutaMantenimiento;}
 	public void setCoutaMantenimiento(double coutaMantenimiento) {this.coutaMantenimiento = coutaMantenimiento;}
 
-	// Constructor
+	// Constructor(1)
 	public CuentaAhorro(Cliente pDuenio, double pMonto)
 	{
 		super ("Cuenta de Ahorro", ++scantCuentas, pDuenio, pMonto);
-		setCoutaMantenimiento(300); // Couta
+		setCoutaMantenimiento(300); // Couta de mantenimiento por omisión
 	}
+	
+	// Contructor (2)
+	public CuentaAhorro (Cliente pDuenio, double pMonto, double pCuota)
+	{
+		super ("Cuenta de Ahorro", ++scantCuentas, pDuenio, pMonto);
+		setCoutaMantenimiento(pCuota);
+	}
+	
+	// Cobro de Comisiones
+	public String cobrarComisiones()
+	{
+		String msg = "";
+		Calendar calendario;
+		calendario = Calendar.getInstance();
+
+		int dia = calendario.get(Calendar.DAY_OF_MONTH);
+		if (dia == 1)
+		{
+			retirar(coutaMantenimiento);
+			numOperaciones = 0; // Se reinicia para el mes siguiente
+			msg = "Se cobró por comisión : " + coutaMantenimiento;
+		}
+		return msg;
+	}
+	
 }
