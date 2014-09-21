3e2c552597ce367fea43b6012d098b55b11bece0
Federico Montealegre <fedemonte.95@gmail.com>
2014-09-21 13:04:25 -0600

    Clases V6 y V8

---
+++ src/logicaDeNegocios/V6.java
@@ -0,0 +1,16 @@
+package logicaDeNegocios;
+
+import java.io.IOException;
+
+public class V6 extends Motor {
+	//atributos
+	int cilindraje = 2500;
+
+	//constructor
+	public V6() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//metodos
+}
---
+++ src/logicaDeNegocios/V8.java
@@ -0,0 +1,20 @@
+package logicaDeNegocios;
+
+
+import java.io.IOException;
+
+import logicaDeNegocios.Motor;
+
+
+public class V8 extends Motor {
+	//atributos 
+	int cilindraje = 3200;
+	
+	//constructor
+	public V8() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//metodos
+}
