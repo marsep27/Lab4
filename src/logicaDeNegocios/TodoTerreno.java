59fd1f2f5fd1aa7478e7dc6c3f38d864f978af7b
untxi <samarburola@gmail.com>
2014-09-21 18:27:07 -0600

    todoTorreno

--- src/logicaDeNegocios/TodoTerreno.java
+++ src/logicaDeNegocios/TodoTerreno.java
@@ -5,19 +5,17 @@ import logicaDeNegocios.Automovil;
 class TodoTerreno extends Automovil {
 
 	// Atributos
-	private int estado;
+	public int estado;
 	//Métodos
 	//constructor 1
 	public TodoTerreno(){
-		int estado = 0;
+		estado = 0;
 	}
 	//constructor 2 
 	public TodoTerreno(int pEstado){
-		int estado = pEstado;
+		estado = pEstado;
 	}
-	public int getEstado() {
-		return estado;
-	}
+	public int getEstado() {return estado;}
 	public void setEstado(int estado) {
 		this.estado = estado;
 	}
